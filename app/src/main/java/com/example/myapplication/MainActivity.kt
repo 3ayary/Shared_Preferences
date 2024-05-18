001265f2d4f6561db1622d81806579bdaf993f1d
muhanad <3ayary@gmail.com>
2024-05-19 01:51:51 +0200

    reformat code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,8 +29,6 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
-
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,24 +37,13 @@ mainui()
             }
         }
     }
-@Preview(showSystemUi = true)
+
 @Composable
 fun mainui() {
     val context = LocalContext.current
     val sharedPreferences = context.getSharedPreferences("myPrefFile", Context.MODE_PRIVATE)
 
-
-    fun saveDataToSharedPreferences(key: String, value: Int) {
-        val editor = sharedPreferences.edit()
-        editor.putInt(key, value)
-        editor.apply() // Use apply() for better performance (consider commit() if immediate persistence is crucial)
-    }
-
-    fun retrieveDataFromSharedPreferences(key: String): String? {
-        return sharedPreferences.getString(key, null)
-    }
-
-    var count = remember {
+    var count by remember {
         mutableStateOf(
             sharedPreferences.getInt("savedCount", 0) // Load initial value
         )
@@ -63,24 +51,21 @@ fun mainui() {
 
     fun saveCount() {
         val editor = sharedPreferences.edit()
-        editor.putInt("savedCount", count.value)
+        editor.putInt("savedCount", count)
         editor.apply() // Use apply() for better performance
     }
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center ,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Count: ${count.value}")
+        Text(text = "Count: ${count}")
         Row {
-            Button(onClick = { count.value++ },enabled = count.value < 100) {
+            Button(onClick = { count++ },enabled = count < 100) {
                 Text("Increment")
                 saveCount()
             }
-            Button(onClick = { if (count.value > 0) count.value-- }, enabled = count.value > 0) {
+            Button(onClick = { if (count > 0) count-- }, enabled = count > 0) {
                 Text("Decrement")
                 saveCount()
             }
         }
     }
-
-
-
 }
